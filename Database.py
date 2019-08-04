d406a7fc51ed76a3d8294776290b8ae608d850cc
Piyush Bhangale <bhangalepiyush@gmail.com>
2019-08-04 22:11:22 +0530

    basic

---
+++ Database.py
@@ -0,0 +1,20 @@
+from pymongo import MongoClient
+import os
+
+
+class Database:
+    def __init__(self):
+        self.ready = False
+
+    def dbSetup(self):
+        self.client = MongoClient(os.getenv("MONGO_URI"))
+        self.db = client["timetable"]
+        self.ready = True
+
+    def new(self, date: str, time: str, subject: str, topic: str):
+        if self.ready is not True:
+            return
+        else:
+            self.db["timetable"].find_one_and_update(
+                {"date": date}, {"$set": {time: time, subject: subject, topic: topic}}
+            )
